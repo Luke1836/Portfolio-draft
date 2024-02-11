d53791af12240dacaf15d028a0d61e33075615f6
Luke1836 <codingluke2023@gmail.com>
2024-02-11 13:34:39 +0530

    Dynamic background setup - 3

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,33 @@
+
+gsap.registerPlugin(ScrollTrigger);
+
 const background = document.querySelector('.background');
+const slider = document.querySelector('.slider');
+
+const sections = gsap.utils.toArray('.slider section');
+const back_ani = gsap.utils.toArray('.background img');
+
+let timeline = gsap.timeline({
+    defaults: {
+        ease: 'none'
+    },
+    ScrollTrigger: {
+        trigger: 'slider',
+        pin: true,
+        scrub: 2,
+        end: () => "+=" + slider.offsetWidth
+    }
+
+})
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+    console.log(e)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
